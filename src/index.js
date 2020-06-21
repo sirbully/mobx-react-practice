2e45819a0d9db89a1bcae02c080fcaecc7465245
Krizza Bullecer <krizzabullecer@gmail.com>
2020-06-22 03:07:23 +0800

    add root store provide

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { RootStoreProvider } from 'stores';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RootStoreProvider>
+      <App />
+    </RootStoreProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
